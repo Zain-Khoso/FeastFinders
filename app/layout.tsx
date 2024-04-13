1ecc216bb435e4ca49f79e8b3879c8cac1171dc8
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-04-13 18:07:31 +0500

    Font & Formating Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,29 @@
+// Lib Imports.
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
+
+// Local Imports.
 import './globals.css';
 
-const inter = Roboto({ weight: '400' });
+// Types.
+type Props = {
+    children: React.ReactNode;
+};
 
+// Font.
+const font = Roboto({ weight: '400', subsets: ['latin'] });
+
+// Metadata
 export const metadata: Metadata = {
     title: 'Feast Finders',
     description: 'All the feasts you need available at one place.',
 };
 
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
+// Component.
+export default function RootLayout({ children }: Readonly<Props>) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={font.className}>{children}</body>
         </html>
     );
 }
