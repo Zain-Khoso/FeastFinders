c94941a83fbdb6768710a0bc9f3b4d2321c5e8fd
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-04-13 18:39:39 +0500

    Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,39 @@ const font = Roboto({ weight: '400', subsets: ['latin'] });
 
 // Metadata
 export const metadata: Metadata = {
-    title: 'Feast Finders',
-    description: 'All the feasts you need available at one place.',
+    title: 'Feast Finder: Explore Culinary Delights & Easy Food Delivery',
+    description:
+        'Discover culinary delights with Feast Finder, your premier destination for seamless food delivery. From gourmet meals to local favorites, explore a diverse range of cuisines curated for your palate. With our intuitive platform, ordering your favorite dishes has never been easier. Join us and indulge in a world of flavor today.',
+    keywords: [
+        'food delivery',
+        'gourmet meals',
+        ' local favorites',
+        ' culinary experience',
+        ' easy ordering',
+        ' diverse cuisines',
+        ' feast finder',
+    ],
+    authors: [
+        {
+            name: 'Feast Finder Team',
+            url: 'https://github.com/Zain-Khoso/FeastFinder.git',
+        },
+    ],
+    robots: { index: true, follow: true },
+    openGraph: {
+        title: 'Feast Finder: Explore Culinary Delights & Easy Food Delivery',
+        description:
+            'Discover culinary delights with Feast Finder, your premier destination for seamless food delivery.',
+        images: '/brand/logo.ico',
+        url: 'https://github.com/Zain-Khoso/FeastFinder.git',
+    },
+    twitter: {
+        title: 'Feast Finder: Explore Culinary Delights & Easy Food Delivery',
+        description:
+            'Discover culinary delights with Feast Finder, your premier destination for seamless food delivery.',
+        images: '/brand/logo.ico',
+        card: 'summary_large_image',
+    },
 };
 
 // Component.
