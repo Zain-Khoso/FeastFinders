929c24d3a8fd1fbb1313ecb1a84ad31211828b1b
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-04-19 23:49:11 +0500

    Signup form first step complete with shadcnui-form react-hook-form zod

---
+++ app/users/auth/signup/individual/Form.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+// Lib Imports.
+import { useState, useReducer } from 'react';
+
+// Local Imports.
+import EmailPhoneCountryCity from '../EmailPhoneCountryCity';
+import reducer from './reducer';
+
+// Component.
+export default function Form() {
+    const [step, setStep] = useState(1);
+    const [state, dispatch] = useReducer(reducer, {});
+
+    const nextStep = () => setStep(step + 1);
+    const prevStep = () => setStep(step - 1);
+
+    if (step === 1)
+        return (
+            <EmailPhoneCountryCity
+                defaultValues={state}
+                dispatch={dispatch}
+                nextStep={nextStep}
+            />
+        );
+
+    // if (step === 2)
+    //     return (
+    //         <CountryCityPhone
+    //             defaultValues={state}
+    //             dispatch={dispatch}
+    //             nextStep={nextStep}
+    //             prevStep={prevStep}
+    //         />
+    //     );
+}
---
+++ app/users/auth/signup/individual/reducer.ts
@@ -0,0 +1,32 @@
+// Types.
+export type State = {
+    account_type: string;
+    email: string;
+    phone: string;
+    country: string;
+    city: string;
+    username: string;
+    password: string;
+    profilePicture: string;
+    coverPicture: string;
+    about_me: string;
+    firstname: string;
+    lastname: string;
+    gender: string;
+    dob: string;
+    address: string;
+};
+export type Action = {
+    fieldName: string;
+    fieldValue: string;
+};
+
+export default function reducer(state: State, action: Action) {
+    const newFeild: any = {};
+    newFeild[action.fieldName] = action.fieldValue;
+
+    return {
+        ...state,
+        ...newFeild,
+    };
+}
