7293ce6d76626d32ca8762d0605d15a1414c58a0
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-05-02 09:11:43 +0500

    fix mobile responsiveness

--- app/users/auth/layout.tsx
+++ app/users/auth/layout.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function Layout({ children }: Props) {
     return (
         <main className="w-full h-[100dvh] flex justify-center items-center primary-gradiant">
-            <section className="w-full max-w-[500px] h-full max-h-[700px] bg-white flex flex-col justify-center align-center gap-8 px-4 md:rounded-xl">
+            <section className="w-full h-full bg-white flex flex-col justify-center align-center gap-8 px-4 md:max-w-[500px] md:max-h-[700px] md:rounded-xl">
                 {children}
             </section>
         </main>
