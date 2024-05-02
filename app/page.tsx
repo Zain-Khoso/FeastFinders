6ada801408369e053771473302d52e3479e809f6
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-05-02 09:50:27 +0500

    new: minimal homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+// Local Imports.
+import { H1, Lead } from '@/components/ui/typography';
+
 export default function Home() {
-    return <h1>Hello World</h1>;
+    return (
+        <div className="w-full h-[100vh] flex flex-col gap-4 justify-center items-center">
+            <H1>Feast Finders</H1>
+            <Lead>This Web App is currently under development.</Lead>
+        </div>
+    );
 }
